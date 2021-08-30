e04246995dbfa6606cff10edb165e82936c3a5f3
aamaulana10 <maulana.yusuf@misteraladin.com>
2021-08-31 00:03:40 +0700

    add home , search, and cart view

---
+++ lib/feature/mainTabbar.dart
@@ -0,0 +1,56 @@
+import 'package:aressa_commerce/feature/cart/view/cartView.dart';
+import 'package:aressa_commerce/feature/home/view/homeView.dart';
+import 'package:aressa_commerce/feature/search/view/searchView.dart';
+import 'package:flutter/material.dart';
+
+class MainTabbar extends StatefulWidget {
+  const MainTabbar({Key key}) : super(key: key);
+
+  @override
+  _MainTabbarState createState() => _MainTabbarState();
+}
+
+class _MainTabbarState extends State<MainTabbar> {
+
+  var screen = [
+    HomeView(),
+    SearchView(),
+    CartView()
+  ];
+
+  var currentIndex = 0;
+
+  void onChangeTab(int index) {
+
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screen[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (e) => { onChangeTab(e) },
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Search"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: "Cart"
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:aressa_commerce/feature/home/view/homeView.dart';
+import 'package:aressa_commerce/feature/mainTabbar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,11 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container()
+      home: MainTabbar()
     );
   }
 }
