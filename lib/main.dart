656d5b9d03d159f02c426339bc4b76fa58e7e219
dafamalik <dafamalik08@gmail.com>
2021-08-31 10:28:06 +0700

    new offerView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aressa_commerce/offer/view/offerView.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container()
+      home: OfferView()
     );
   }
 }
---
+++ lib/offer/view/offerView.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OfferView extends StatefulWidget {
+
+  @override
+  _OfferViewState createState() => _OfferViewState();
+}
+
+class _OfferViewState extends State<OfferView> {
+
+  Widget appBar() {
+    return Container(
+      height: 35,
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.only(top: 32, left: 16, right: 16),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 1, color: Colors.grey),
+            )
+        ),
+        child: Text("Offer",
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget content() {
+    return Container(
+      margin: EdgeInsets.only(left: 16, right: 16),
+      child: Column(
+        children: [
+          Container(
+            height: 80,
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.only(top: 16),
+            padding: EdgeInsets.only(left: 16, right: 16),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(8)
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Use “MEGSL” Cupon For Get 90%off",
+                    maxLines: 2,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16),
+            child: Stack(
+              children: [
+                Positioned(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image(
+                        image: AssetImage("asset/image/PromotionImage1.png"),
+                        height: 206,
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                Positioned(
+                  top: 32,
+                    left: 24,
+                    child: Container(
+                      width: 209,
+                      child: Column(
+                        children: [
+                          Text("Super Flash Sale 50% Off",
+                          maxLines: 2,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold
+                          ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 30),
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 41,
+                                  width: 42,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(8)
+                                  ),
+                                  child: Center(
+                                    child: Text("08",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold
+                                    ),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(left: 8, right: 8),
+                                  child: Text(":",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                  ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 41,
+                                  width: 42,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(8)
+                                  ),
+                                  child: Center(
+                                    child: Text("34",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(left: 8, right: 8),
+                                  child: Text(":",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 41,
+                                  width: 42,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(8)
+                                  ),
+                                  child: Center(
+                                    child: Text("52",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16),
+            child: Stack(
+              children: [
+                Positioned(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image(
+                        image: AssetImage("asset/image/PromotionImage2.png"),
+                        height: 206,
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                Positioned(
+                    top: 32,
+                    left: 24,
+                    child: Container(
+                      width: 209,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("90% Off Super Mega Sale",
+                            maxLines: 2,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 32),
+                            child: Text("Special birthday Lafyuu",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12
+                            ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              appBar(),
+              content(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
